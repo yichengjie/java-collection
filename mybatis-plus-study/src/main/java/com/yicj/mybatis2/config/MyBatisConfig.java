fff28b3ce82dabcc070ec91ede8d0ea8fdd76b58
yichengjie <626659321@qq.com>
2019-05-29 17:42:29 +0800

    优化代码

--- mybatis-plus-study/src/main/java/com/yicj/mybatis2/config/MyBatisConfig.java
+++ mybatis-plus-study/src/main/java/com/yicj/mybatis2/config/MyBatisConfig.java
@@ -4,6 +4,7 @@ import java.util.Properties;
 import javax.sql.DataSource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -18,8 +19,20 @@ import com.alibaba.druid.pool.DruidDataSourceFactory;
 @MapperScan("com.yicj.mybatis2.mapper")
 public class MyBatisConfig {
 	
-	@Autowired
-	private Environment env ;
+	@Value("${jdbc.driverClassName}")
+	private String driverClassName ;
+	//
+	@Value("${jdbc.url}")
+	private String url  ;
+	//
+	@Value("${jdbc.username}")
+	private String username ;
+	//
+	@Value("${jdbc.password}")
+	private String password ;
+	//可以通过env.getProperties("key")获取键值对
+	//@Autowired
+	//private Environment env ;
 	/**
 	 * 创建数据源
 	 * @Primary 该注解表示在同一个接口有多个实现类可以注入的时候，默认选择哪一个，而不是让@autowire注解报错 
@@ -30,10 +43,10 @@ public class MyBatisConfig {
 	@Bean
 	public DataSource getDataSource() throws Exception {
 		Properties props = new Properties() ;
-		props.put("driverClassName", env.getProperty("jdbc.driverClassName")) ;
-		props.put("url", env.getProperty("jdbc.url")) ;
-		props.put("username", env.getProperty("jdbc.username")) ;
-		props.put("password", env.getProperty("jdbc.password")) ;
+		props.put("driverClassName", driverClassName) ;
+		props.put("url", url) ;
+		props.put("username", username) ;
+		props.put("password", password) ;
 		return DruidDataSourceFactory.createDataSource(props) ;
 	}
 }
